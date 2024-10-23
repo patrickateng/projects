9ae6594e15e7f9d32e6367e1f080e5cf24b446d8
patrickateng <99479428+patrickateng@users.noreply.github.com>
2024-10-23 11:35:56 +0300

    projects

---
+++ e-commerce-website/js/main.js
@@ -0,0 +1,16 @@
+// Cart
+let cartIcon = document.querySelector('#cart-icon')
+let cart = document.querySelector('.cart')
+let closeCart = document.querySelector('#close-cart')
+
+// Open Cart
+cartIcon.onclick = () => {
+  cart.classList.add("active")
+}
+
+// Close Cart
+closeCart.onclick = () => {
+  cart.classList.remove("active")
+}
+
+// art Working JS
